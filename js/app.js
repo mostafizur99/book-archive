51bf388e5c394c502ac450bd948a81c555226f56
mostafizur99 <coder.mr11@gmail.com>
2021-09-02 18:24:47 +0600

    js added

---
+++ js/app.js
@@ -0,0 +1,85 @@
+
+const searchBook = () => {
+    // clear-previous displayed items
+    document.getElementById("error-message").textContent = '';
+    document.getElementById('search-result').textContent = '';
+    document.getElementById('seach-number').textContent = '';
+
+    const searchField = document.getElementById('search-input');
+    const searchText = searchField.value;
+    searchField.value = '';
+
+    if (searchText.length > 0) {
+        document.getElementById("spinner").classList.remove("d-none");
+        const url = `https://openlibrary.org/search.json?q=${searchText}`;
+        fetchedData(url)
+            .then(data => displaySearchResult(data));
+    } else {
+        document.getElementById("error-message").innerHTML =
+            "<p class='text-center p-3 bg-danger'><b>Please enter a book name...</b></p>";
+    }
+};
+
+// function for data fetching 
+const fetchedData = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+    return data;
+};
+
+const displaySearchResult = data => {
+    // turn of spinner
+    document.getElementById("spinner").classList.add("d-none");
+
+    const { docs, numFound } = data;
+
+    // search-result number
+    const searchResultNumber = document.getElementById('seach-number');
+    if (numFound === 1) {
+        searchResultNumber.innerHTML = `<span class="text-color">${numFound} result found</span>`;
+    } else if (numFound > 1) {
+        searchResultNumber.innerHTML = `<span class="text-color">${numFound} results found</span>`;
+    } else {
+        searchResultNumber.innerHTML = `<span class="text-danger">Sorry, no result found</span>`;
+    }
+
+    // showing result for 27 books 
+    const searchResult = document.getElementById('search-result');
+    const showedResult = docs.slice(0, 27);
+
+    showedResult.forEach(book => {
+        const { title, author_name, first_publish_year, publisher, cover_i } = book;
+
+        // display portions with default value
+        const firstPublisher = publisher?.slice(0, 1);
+        const firstAuthor = author_name?.slice(0, 1);
+        const loadSearch = (thumbUrl, firstAuthor = 'no author found', first_publish_year = 'no year found', firstPublisher = 'no publisher found') => {
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `
+                <div class="card h-100 shadow">
+                    <img src="${thumbUrl}" class="card-img-top">
+                    <div class="card-body">
+                        <h6 class="card-title"><b>Book Name: <span class="text-color">${title}</span></b></h6>
+                        <h6 class="card-title">Author: <span class="fst-italic text-color">${firstAuthor}</span></h6>
+                        <h6 class="card-title">First published: <span class="text-color">${first_publish_year}</span></h6>
+                    </div>
+                    <div class="card-footer custom-bg">
+                        <h6 class="card-title">Publisher: <span class="text-color">${firstPublisher}</span></h6>
+                    </div>
+                </div>
+        `;
+            searchResult.appendChild(div);
+        };
+        // default book-thumbnail 
+        if (cover_i !== undefined) {
+            const thumbUrl = `https://covers.openlibrary.org/b/id/${cover_i}-M.jpg`;
+            loadSearch(thumbUrl, firstAuthor, first_publish_year, firstPublisher);
+        } else {
+            const thumbUrl = './image/no-image.jpg';
+            loadSearch(thumbUrl, firstAuthor, first_publish_year, firstPublisher);
+
+        }
+
+    });
+};
